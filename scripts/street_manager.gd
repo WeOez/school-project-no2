ed0741a0fa1a07ad25e9d712841beaa40c5fbf61
WeOez <154280069+WeOez@users.noreply.github.com>
2025-10-12 01:17:35 +0300

    added street picker

---
+++ scripts/street_manager.gd
@@ -0,0 +1,23 @@
+extends Node2D
+@export var options: Array[Node2D]
+
+signal correct(street)
+signal mistake(street)
+
+func _ready() -> void:
+	for i in options:
+		i.option_picked.connect(check)
+		
+func check(id: int, option: Button, picker_id: int, self_button: Button):
+	match id:
+		1, 2, 3:
+			var index = id - 1
+			if index == picker_id:
+				var tween = create_tween()
+				tween.tween_property(self_button, "modulate", Color(0.443, 0.506, 0.204, 1), 0.2)
+				self_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+				correct.emit(id)
+			else:
+				var tween = create_tween()
+				tween.tween_property(self_button, "modulate", Color.RED, 0.2)
+				mistake.emit(id)
--- scripts/street_picker.gd
+++ scripts/street_picker.gd
@@ -1,19 +1,39 @@
 extends Node2D
 
-@export var names: Array[Label]
+@export var names: Array[Button]
 @export var button: Button
+@export var button_text: Label
 var id: int
+@export var correct_id: int
 var offset: float
+var mouse_overlaps_option = false
+var original_rotation: float
 
-#а теперь добавим всему анимации
+signal option_picked(option_id: int, option: Button, picker_id: int, self_button: Button)
+signal button_pressed(button_id: int)
 
 func _ready() -> void:
-	var tween = create_tween()
 	for i in names:
-		i.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		i.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+		i.visible = false
 		i.modulate = Color(1.0, 1.0, 1.0, 0.0)
+		i.pressed.connect(_on_option_picked.bind(i))
+		i.mouse_entered.connect(_on_mouse_entered_option)
+		i.mouse_exited.connect(_on_mouse_exited_option)
+	original_rotation = self.rotation_degrees
 	get_label_data()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if event.is_pressed() and not mouse_overlaps_option:
+			var tween = create_tween()
+			tween.tween_property(self, "rotation_degrees", original_rotation, 0.1)
+			button.visible = true
+			button_text.visible = true
+			
+	
+			for i in names:
+				i.visible = false
+				tween.tween_property(i, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.15)
 
 func get_label_data():
 	for i in names:
@@ -33,9 +53,35 @@ func get_label_data():
 				names[2].text = "Солнечная Улица"
 
 func _on_pressed():
-	button.visible = false
 	var tween = create_tween()
-	
+	tween.tween_property(self, "rotation_degrees", 0.0, 0.1)
+	button.visible = false
+	button_text.visible = false
 	for i in names:
+		i.visible = true
 		tween.tween_property(i, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.15)
-		#i.visible = true
+		
+func _on_area_entered(area):
+	button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+func _on_area_exited(area):
+	button.mouse_filter = Control.MOUSE_FILTER_STOP
+
+func _on_option_picked(option):
+	var tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", original_rotation, 0.1)
+	for i in names:
+		i.visible = false
+		tween.tween_property(i, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.15)
+
+	button.visible = true
+	button_text.visible = true
+	button_text.text = option.text
+	
+	option_picked.emit(option.get_index(), option, self.get_index(), button)
+	
+func _on_mouse_entered_option():
+	mouse_overlaps_option = true
+	
+func _on_mouse_exited_option():
+	mouse_overlaps_option = false
